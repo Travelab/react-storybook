7f05b73e4f0294ee948cd0eaf168cfd2d89e9597
spoluektov <spoluer@gmail.com>
2018-04-13 07:04:30 +0300

    chore: update dist

--- dist/client/preview/error_display.js
+++ dist/client/preview/error_display.js
@@ -8,6 +8,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mainStyle = {
@@ -59,7 +63,7 @@ var ErrorDisplay = function ErrorDisplay(_ref) {
 };
 
 ErrorDisplay.propTypes = {
-  error: _react2.default.PropTypes.object.isRequired
+  error: _propTypes2.default.object.isRequired
 };
 
 exports.default = ErrorDisplay;
